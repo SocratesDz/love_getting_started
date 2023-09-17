f67b86d4677d3c34f9b7d8d19d8ea7e5fea9749a
Sócrates Díaz <sdsxd1800@gmail.com>
2023-09-17 17:39:17 -0400

    main.lua: Add the feature to load any module in the project.

--- main.fnl
+++ main.fnl
@@ -1,5 +1,6 @@
 (fn love.draw []
-  (love.graphics.print "Hello from Fennel!\nPress any key to quit" 10 10))
+  (love.graphics.print "Hello from Fennel!\nPress any key to quit" 10 10)
+  (love.graphics.print "Another printed line" 10 80))
 
 (fn love.keypressed [key]
   (love.event.quit))
--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
-require("fennel").eval(love.filesystem.read("main.fnl"), {env=_G})
+fennel = require("fennel")
+debug.traceback = fennel.traceback
+table.insert(package.loaders, function(filename)
+                if love.filesystem.getInfo(filename) then
+                   return function(...)
+                      return fennel.eval(love.filesystem.read(filename), {env=_G, filename=filename}, ...), filename
+                   end
+                end
+end)
+
+require("main.fnl")
